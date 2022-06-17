fb8e93177dbe1d1e1628a19e95de17a3a5a959ce
Ivan Hernandez <ivan.hernandez.coral@gmail.com>
2022-06-17 12:25:16 -0500

    Probando IFRAME 2

--- src/containers/articulos/Menu.js
+++ src/containers/articulos/Menu.js
@@ -10,7 +10,8 @@ function Menu()
       <Header height={"none"} fondo={""} titulo={""}/>
       <h1>Menuuuu</h1>
 
-      <iframe src="https://83n21dw79d3imcg.us.qlikcloud.com/single/?appid=81505c41-8e9e-436d-b179-014d26f0c651&obj=RpXDxu&opt=ctxmenu,currsel&select=$::dimensiones,Productos&select=$::categoria,Grupo&select=$::periodicidad,Mes&select=$::unidad,Cantidad&select=$::a%C3%B1o,2022" style="border:none;width:100%;height:100%;"></iframe>
+      <iframe src="https://public.domo.com/cards/W6m2W" width="900" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+      {/* <iframe src="https://83n21dw79d3imcg.us.qlikcloud.com/single/?appid=81505c41-8e9e-436d-b179-014d26f0c651&obj=RpXDxu&opt=ctxmenu,currsel&select=$::dimensiones,Productos&select=$::categoria,Grupo&select=$::periodicidad,Mes&select=$::unidad,Cantidad&select=$::a%C3%B1o,2022" style="border:none;width:100%;height:100%;"></iframe> */}
     </div>
   );
 }
@@ -18,3 +19,4 @@ function Menu()
 export default Menu;
 
 // https://acuatex-web.herokuapp.com
+
