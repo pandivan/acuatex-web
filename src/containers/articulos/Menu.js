66d3787618956704a0b4874ce372a880102ef36a
Ivan Hernandez <ivan.hernandez.coral@gmail.com>
2022-06-17 11:47:44 -0500

    Probando IFRAME 1

--- src/containers/articulos/Menu.js
+++ src/containers/articulos/Menu.js
@@ -9,8 +9,12 @@ function Menu()
     <div>
       <Header height={"none"} fondo={""} titulo={""}/>
       <h1>Menuuuu</h1>
+
+      <iframe src="https://83n21dw79d3imcg.us.qlikcloud.com/single/?appid=81505c41-8e9e-436d-b179-014d26f0c651&obj=RpXDxu&opt=ctxmenu,currsel&select=$::dimensiones,Productos&select=$::categoria,Grupo&select=$::periodicidad,Mes&select=$::unidad,Cantidad&select=$::a%C3%B1o,2022" style="border:none;width:100%;height:100%;"></iframe>
     </div>
   );
 }
 
 export default Menu;
+
+// https://acuatex-web.herokuapp.com
